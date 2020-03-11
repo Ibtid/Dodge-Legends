7913a4d9773094229da19052199939185634d8bd
Ibtid <56163318+Ibtid@users.noreply.github.com>
2020-03-11 20:12:19 +0600

    Update BasicEnemy.java

--- BasicEnemy.java
+++ BasicEnemy.java
@@ -1,5 +1,6 @@
 package dodge.legends;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
@@ -7,21 +8,30 @@ public class BasicEnemy extends GameObjects{
 
     public BasicEnemy(int x, int y, ID id) {
         super(x, y, id);
+        velocityX = 5;
+        velocityY = 5;
     }
 
     @Override
     public void tick() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        x+=velocityX;
+        y+=velocityY;
+        
+        if(y<10 || y>=DodgeLegends.height-60) velocityY *= -1;
+        if(x<0 || x>=DodgeLegends.width-40) velocityX *= -1;
     }
 
     @Override
     public void render(Graphics graphics) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Rectangle getbounds() {
-        return new Rectangle (x, y, 16, 16);
+        graphics.setColor(Color.red);
+        graphics.fillOval(x, y, 30, 30);
+        
     }
     
+    @Override
+    public Rectangle getbounds() {
+ 
+        return new Rectangle (x, y, 16, 16);
+    }
+
 }
