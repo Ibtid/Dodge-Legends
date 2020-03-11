68674d87da9545bfba096576a9d850d214d5cdd7
Ibtid <56163318+Ibtid@users.noreply.github.com>
2020-03-11 20:16:42 +0600

    Update KeyInput.java

--- KeyInput.java
+++ KeyInput.java
@@ -1,18 +1,19 @@
-
 package dodge.legends;
 
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-public class KeyInput extends KeyAdapter {
+public class KeyInput extends KeyAdapter{
 
     private Handler handler;
+    Player player;
        
         public KeyInput(Handler handler){
             this.handler = handler;
         }
        
-        public void KeyPressed(KeyEvent e){
+    @Override
+        public void keyPressed(KeyEvent e){
             int key = e.getKeyCode();
            
             for(int i=0; i < handler.object.size(); i++){
@@ -20,17 +21,25 @@ public class KeyInput extends KeyAdapter {
                
                 if(tempObject.getId() == ID.Player){
                     //key events for player 1
-                    if (key == KeyEvent.VK_W) tempObject.setVelocityY(-5);
-                    if (key == KeyEvent.VK_S) tempObject.setVelocityY(5);
-                    if (key == KeyEvent.VK_D) tempObject.setVelocityY(5);
-                    if (key == KeyEvent.VK_A) tempObject.setVelocityY(-5);
+                    if (key == KeyEvent.VK_W) 
+                       tempObject.setVelocityY(-5);
+       
+                    if (key == KeyEvent.VK_S)
+                        tempObject.setVelocityY(5);
+                   
+                    if (key == KeyEvent.VK_D) 
+                       tempObject.setVelocityX(5);
+                    
+                    if (key == KeyEvent.VK_A) 
+                       tempObject.setVelocityX(-5);
+                     
                 }
             }
-           
+            if(key == KeyEvent.VK_ESCAPE)System.exit(1);
          
-                   
         }
-        public void KeyReleased(KeyEvent e){
+    @Override
+        public void keyReleased(KeyEvent e){
           int key = e.getKeyCode();
            
             for(int i=0; i < handler.object.size(); i++){
@@ -40,8 +49,8 @@ public class KeyInput extends KeyAdapter {
                     //key events for player 1
                     if (key == KeyEvent.VK_W) tempObject.setVelocityY(0);
                     if (key == KeyEvent.VK_S) tempObject.setVelocityY(0);
-                    if (key == KeyEvent.VK_D) tempObject.setVelocityY(0);
-                    if (key == KeyEvent.VK_A) tempObject.setVelocityY(0);
+                    if (key == KeyEvent.VK_D) tempObject.setVelocityX(0);
+                    if (key == KeyEvent.VK_A) tempObject.setVelocityX(0);
                 }
                  
             }
