475ed2dd08f313739d50b6db6d79e0f7a5a665bd
Ibtid <56163318+Ibtid@users.noreply.github.com>
2020-03-23 21:02:12 +0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -10,14 +10,16 @@ import java.util.Random;
 public class Player extends GameObjects{
     
     Random r = new Random();
+    Handler handler;
 
-    public Player(int x, int y, ID id) {
+    public Player(int x, int y, ID id, Handler handler) {
         super(x, y, id);
+        this.handler = handler;
         
     }
     
     @Override
-    public Rectangle getbounds(){
+    public Rectangle getBounds(){
         return new Rectangle(x, y, 32, 32);
     }
 
@@ -27,12 +29,31 @@ public class Player extends GameObjects{
         y += velocityY;
         
         x = DodgeLegends.clamp(x, 0, DodgeLegends.width-40);
-        y = DodgeLegends.clamp(y, 0, DodgeLegends.height-80);
+        y = DodgeLegends.clamp(y, 0, DodgeLegends.height-75);
         
-        /*if(y>=DodgeLegends.height-80) y = DodgeLegends.height-80 ;
-        if(x>=DodgeLegends.width-40) x = DodgeLegends.width-40; 
-        if(y<7) y = 7;
-        if(x<0) x = 0;*/
+        handler.addObject(new Trail(x, y, ID.Trail, Color.yellow, 0.05f, 32, 32, handler ));
+        
+        collision();
+        
+    }
+    
+    private void collision(){
+        
+        for(int i = 0; i < handler.object.size(); i++){
+            
+            GameObjects tempObject = handler.object.get(i);
+            
+            if(tempObject.getId() == ID.BasicEnemy){
+                
+                if(getBounds().intersects(tempObject.getBounds())){
+                    HUD.HEALTH-=2;
+                    tempObject.velocityX *= -1;
+                    tempObject.velocityY *= -1;
+                }
+                
+            }
+        }
+        
     }
     
 
@@ -41,7 +62,6 @@ public class Player extends GameObjects{
         
         graphics.setColor(Color.yellow);
         graphics.fillOval(x, y, 32, 32);
-        
     }
     
 }
