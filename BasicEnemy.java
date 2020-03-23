a0c028e9b4cb7cfbb235fc09f48062247b0da17c
Ibtid <56163318+Ibtid@users.noreply.github.com>
2020-03-23 20:54:22 +0600

    Update BasicEnemy.java

--- BasicEnemy.java
+++ BasicEnemy.java
@@ -6,8 +6,13 @@ import java.awt.Rectangle;
 
 public class BasicEnemy extends GameObjects{
 
-    public BasicEnemy(int x, int y, ID id) {
+    private Handler handler;
+    
+    public BasicEnemy(int x, int y, ID id, Handler handler) {
         super(x, y, id);
+        
+        this.handler = handler;
+        
         velocityX = 5;
         velocityY = 5;
     }
@@ -19,17 +24,19 @@ public class BasicEnemy extends GameObjects{
         
         if(y<10 || y>=DodgeLegends.height-60) velocityY *= -1;
         if(x<0 || x>=DodgeLegends.width-40) velocityX *= -1;
+        
+        handler.addObject(new Trail(x, y, ID.Trail, Color.red, 0.02f, 16, 16, handler ));
     }
 
     @Override
     public void render(Graphics graphics) {
         graphics.setColor(Color.red);
-        graphics.fillOval(x, y, 30, 30);
+        graphics.fillOval(x, y, 16, 16);
         
     }
     
     @Override
-    public Rectangle getbounds() {
+    public Rectangle getBounds() {
  
         return new Rectangle (x, y, 16, 16);
     }
