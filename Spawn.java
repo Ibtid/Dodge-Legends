2bc6ce654e7099ba5132885d8161588c6426c242
Ibtid <56163318+Ibtid@users.noreply.github.com>
2020-04-02 00:27:30 +0600

    Update Spawn.java

--- Spawn.java
+++ Spawn.java
@@ -20,14 +20,15 @@ public class Spawn {
             scoreKeep = 0;
             hud.setLevel(hud.getLevel()+1);
             
-            if(hud.getLevel()== 2){
+            if(hud.getLevel() == 2 || hud.getLevel() == 3){
                 handler.addObject(new BasicEnemy(DodgeLegends.width/34, DodgeLegends.height/40, ID.BasicEnemy, handler));
-            }else if(hud.getLevel()== 3){
-                handler.addObject(new BasicEnemy(DodgeLegends.width/34, DodgeLegends.height/40, ID.BasicEnemy, handler));
-            }else if(hud.getLevel()== 4){
+            }else if(hud.getLevel() == 4 || hud.getLevel() == 6){
                 handler.addObject(new FastEnemy(DodgeLegends.width/34, DodgeLegends.height/40, ID.FastEnemy, handler));
-            }else if(hud.getLevel()== 5){
+            }else if(hud.getLevel() == 5 || hud.getLevel() == 7){
                 handler.addObject(new TrapEnemy(DodgeLegends.width/34, DodgeLegends.height/40, ID.TrapEnemy, handler));
+            }else if(hud.getLevel() == 10){
+                handler.clearEnemies();
+                handler.addObject(new BossEnemy(280,-120,ID.BossEnemy,handler));
             }
         }
     }
