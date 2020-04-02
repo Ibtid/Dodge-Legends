0ccca2550cff420896dca37c6453b102e085bd2a
Ibtid <56163318+Ibtid@users.noreply.github.com>
2020-04-02 19:26:14 +0600

    Update BossEnemy.java

--- BossEnemy.java
+++ BossEnemy.java
@@ -6,6 +6,9 @@ import java.awt.Rectangle;
 import java.util.Random;
 
 public class BossEnemy extends GameObjects{
+    
+    int health = 125;
+    int green = 255; ;
 
     private Handler handler;
     Random r = new Random();
@@ -24,6 +27,10 @@ public class BossEnemy extends GameObjects{
 
     @Override
     public void tick() {
+        
+        health = DodgeLegends.clamp(health, 0, 125);
+        green = DodgeLegends.clamp(green, 0, 255);
+        
         x+=velocityX;
         y+=velocityY;
         
@@ -40,17 +47,27 @@ public class BossEnemy extends GameObjects{
             if(spawn == 0) 
                 handler.addObject(new BulletBossEnemy( x, y+40, ID.BulletBossEnemy, handler));
         }
+            
+        if(x<0 || x>=DodgeLegends.width-60) {
+            velocityX *= -1;
+            velocityY = 20;
+            if(y<10 || y>=DodgeLegends.height-120){
+                velocityY = -300;
+            }
+        }
+                   
+        handler.addObject(new Trail(x, y, ID.Trail, new Color(178,102,255), 0.3f, 60, 60, handler ));
         
-       //if(y<10 || y>=DodgeLegends.height-80) velocityY *= -1;
-        if(x<0 || x>=DodgeLegends.width-60) velocityX *= -1;
-        
-        //handler.addObject(new Trail(x, y, ID.Trail, new Color(204,0,0), 0.02f, 60, 60, handler ));
+        collision();
     }
 
     @Override
     public void render(Graphics graphics) {
         graphics.setColor(new Color(178,102,255));
         graphics.fillOval(x, y, 60, 60);
+        
+        drawHUD (graphics);
+        
     }
     
     @Override
@@ -58,6 +75,30 @@ public class BossEnemy extends GameObjects{
  
         return new Rectangle (x, y, 60, 60);
     }
-
     
+    private void drawHUD(Graphics graphics){
+         //Boss HUD
+        graphics.setColor(Color.gray);
+        graphics.fillRect(220, 400, 250, 32);
+        graphics.setColor(new Color( 255, 50, 100));
+        graphics.fillRect(220, 400, health * 2, 32);
+        graphics.setColor(Color.black);
+        graphics.drawRect(220, 400, 250, 32);
+    }
+    
+    private void collision(){
+        
+        for(int i = 0; i < handler.object.size(); i++){
+            
+            GameObjects tempObject = handler.object.get(i);
+            
+            if(tempObject.getId() == ID.Player){
+                
+                if(getBounds().intersects(tempObject.getBounds())){
+                    health-=2;
+                    green-=2;
+                }           
+            }
+        } 
+    }
 }
