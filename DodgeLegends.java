c784ed043c060f92245a0163bd77778ff6c4aeb6
Ibtid <56163318+Ibtid@users.noreply.github.com>
2020-04-10 00:34:37 +0600

    Update DodgeLegends.java

--- DodgeLegends.java
+++ DodgeLegends.java
@@ -1,4 +1,4 @@
-package dodge.legends;
+ package dodge.legends;
 
 import java.awt.Canvas;
 import java.awt.Color;
@@ -15,19 +15,44 @@ public class DodgeLegends extends Canvas implements Runnable{
     private Random r;
     private HUD hud;
     private Spawn spawner;
+    private Menu menu;
+    Music music;
+    
+    public enum STATE {
+        Menu,
+        Help,
+        Game,
+        End
+    };
+    
+    public static STATE gameState = STATE.Menu;
     
     public DodgeLegends(){
         
+        hud = new HUD();
         handler = new Handler();
+        menu = new Menu(this, handler, hud);
         this.addKeyListener(new KeyInput(handler));
+        this.addMouseListener(menu);
+        
         Window window = new  Window(width, height, "Dodge Legends", this);
         
-        hud = new HUD();
+        
         spawner = new Spawn(handler, hud);
         r = new Random();
         
-        handler.addObject(new BasicEnemy(width/34, height/40, ID.BasicEnemy, handler));
-        handler.addObject(new Player(width/2-32,height/2-32, ID.Player, handler));
+        if(gameState == STATE.Game){
+            handler.clearEnemies();
+            //handler.addObject(new BasicEnemy(width/34, height/40, ID.BasicEnemy, handler));
+            //handler.addObject(new Player(width/2-32,height/2-32, ID.Player, handler));
+        }else{
+            for(int i = 0; i<10; i ++){
+                handler.addObject(new MenuParticle(r.nextInt(width), r.nextInt(height), ID.MenuParticle, handler));
+            
+            }
+        }
+        
+        
     }
     
     public synchronized void start(){
@@ -56,6 +81,7 @@ public class DodgeLegends extends Canvas implements Runnable{
         double delta = 0;
         long timer = System.currentTimeMillis();
         int frames = 0;
+        music.playMusic("Music\\\\bgmusic.wav");
         while(running)
         {
             long now = System.nanoTime();
@@ -81,8 +107,25 @@ public class DodgeLegends extends Canvas implements Runnable{
     
     private void tick(){
         handler.tick();
-        hud.tick();
-        spawner.tick();
+        
+        if(gameState == STATE.Game){    
+            hud.tick();
+            spawner.tick();
+            if(HUD.playerHEALTH<=0){
+                //HUD.playerHEALTH = 100;
+                //hud.setScore(0);
+                //hud.setLevel(1);
+                gameState = STATE.End;
+                handler.clearEnemies(); 
+                for(int i = 0; i<10; i ++){
+                handler.addObject(new MenuParticle(r.nextInt(width), r.nextInt(height), ID.MenuParticle, handler));
+            
+                }
+            }
+        }else if(gameState == STATE.Menu || gameState == STATE.End){
+            menu.tick();
+        }
+            
     }
     
     private void render(){
@@ -95,12 +138,26 @@ public class DodgeLegends extends Canvas implements Runnable{
         
         Graphics graphics = bufferstrategy.getDrawGraphics();
             
-        
-        graphics.setColor(new Color(224,224,224));
-        graphics.fillRect(0, 0, width, height);
+        if(gameState == STATE.Game){
+            graphics.setColor(new Color(224,224,224));
+            graphics.fillRect(0, 0, width, height);
+        }else{
+            graphics.setColor(new Color(0,0,0));
+            graphics.fillRect(0, 0, width, height);
+        }
+       
         
         handler.render(graphics);
-        hud.render(graphics);
+        
+        if(gameState == STATE.Game){
+            
+            hud.render(graphics);
+            
+        }else if(gameState == STATE.Menu || gameState == STATE.Help || gameState == STATE.End){
+            
+            menu.render(graphics);
+            
+        }
         
         
         graphics.dispose();
